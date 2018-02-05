779e2bc6422b08ece63955f3ae2f39558961b97f
Cabesa <alejandrocabesa@hotmail.com>
2018-02-05 11:57:52 +0100

    Creación clase Restaurante

---
+++ app/src/main/java/com/example/aleja/comigram/data/Restaurante.java
@@ -0,0 +1,95 @@
+package com.example.aleja.comigram.data;
+
+import java.util.UUID;
+
+/**
+ Entidad Restaurante
+ */
+
+public class Restaurante {
+    private String id;
+    private String nombre;
+    private String categoria;
+    private String longitud;
+    private String latitud;
+    private String valoracion;
+    private String descripcion;
+    private String foto;
+
+    public Restaurante(String nombre, String categoria, String longitud, String latitud, String valoraciom, String descripcion, String foto )
+    {
+        this.id = UUID.randomUUID().toString();
+        this.nombre=nombre;
+        this.categoria=categoria;
+        this.descripcion=descripcion;
+        this.longitud=longitud;
+        this.latitud=latitud;
+        this.valoracion=valoracion;
+        this.foto=foto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(String valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
+
