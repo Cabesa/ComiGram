1192bd5507fd400d5cd8dc2d25481892b47cc8e4
Cabesa <alejandrocabesa@hotmail.com>
2018-02-26 12:14:40 +0100

    Subida version 2.0

---
+++ Definitiva/app/src/main/java/com/example/aleja/definitiva/EditarRestauranteActivity.java
@@ -0,0 +1,464 @@
+package com.example.aleja.definitiva;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;;import java.io.BufferedInputStream;
+import java.io.File;
+
+public class EditarRestauranteActivity extends AppCompatActivity implements OnMapReadyCallback,LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String ARG_RESTAURANTE_ID = "arg_restaurante_id";
+    private String mRestauranteId;
+    private RestauranteDbHelper mRestaurantesDbHelper;
+    private String categorias[];
+    TextView txtnombre;
+    GoogleMap mMap;
+    private double latitud=0;
+    private double longitud=0;
+
+    private double longitudfinal=0;
+    private double latitudfinal=0;
+    private Button btnMap;
+    RatingBar bar;
+    final String TAG = "GPS";
+    private long UPDATE_INTERVAL = 2 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    GoogleApiClient gac;
+    LocationRequest locationRequest;
+
+    private static int DESDE_CAMARA = 1;
+    private static int DESDE_GALERIA = 2;
+    private ImageView img;
+    private String foto;
+    private Spinner spinner;
+    private EditText txtdescripcion;
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_restaurante);
+        btnMap=(Button)findViewById(R.id.btnMapaEdit);
+        isGooglePlayServicesAvailable();
+
+        if(!isLocationEnabled())
+            showAlert();
+
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        gac = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        img=(ImageView)findViewById(R.id.imgEdit);
+        bar=(RatingBar)findViewById(R.id.barraValoracionEdit);
+        mRestauranteId = getIntent().getStringExtra(RestaurantesActivity.EXTRA_RESTAURANTE_ID);
+        txtnombre=(TextView)findViewById(R.id.txtNombreEdit) ;
+        txtdescripcion=(EditText)findViewById(R.id.txtDescripcionEdit);
+        categorias = getResources().getStringArray(R.array.categorias);
+        spinner = (Spinner) findViewById(R.id.spinCategoriaEdit);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item, categorias);
+        spinner.setAdapter(spinnerArrayAdapter);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabEdit);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editRestaurante();
+            }
+        });
+        mRestaurantesDbHelper=new RestauranteDbHelper(getApplicationContext());
+
+        loadRestaurante();
+
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.mapEditar, mMapFragment);
+        fragmentTransaction.commit();
+        mMapFragment.getMapAsync(this);
+        configure_button();
+    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng rest = new LatLng(latitudfinal, longitudfinal);
+        mMap.addMarker(new MarkerOptions().position(rest).title(String.valueOf(txtnombre.getText())));
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(rest)
+                .zoom(11)
+                .build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+    }
+    private void loadRestaurante() {
+        new GetLawyerByIdTask().execute();
+    }
+
+    private void showRestaurante(Restaurante restaurante) {
+
+        txtnombre.setText(restaurante.getnombre());
+        latitudfinal=Double.parseDouble(restaurante.getlatitud());
+        longitudfinal=Double.parseDouble(restaurante.getlongitud());
+        bar.setRating(Float.parseFloat(restaurante.getvaloracion()));
+        foto=restaurante.getfoto();
+        txtdescripcion.setText(restaurante.getdescripcion());
+        for(int i=0;i<categorias.length;i++)
+        {
+            if(categorias[i].equals(restaurante.getcategoria()))
+                spinner.setSelection(i);
+        }
+        Bitmap icon=null;
+        if(foto.equals(""))
+        {
+            icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                    R.drawable.sinimagen);
+        }
+        else if(foto.startsWith("/"))
+        {
+            File imgFile = new File(foto);
+            if(imgFile.exists()) {
+                icon=BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            }
+            else {
+                icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                        R.drawable.sinimagen);
+            }
+        }
+        else
+        {
+            icon=StringBitmap.StringToBitMap(foto);
+        }
+        img.setImageBitmap(icon);
+    }
+
+    private void showLoadError() {
+        Toast.makeText(getApplicationContext(),
+                "Error al cargar información", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+    private class GetLawyerByIdTask extends AsyncTask<Void, Void, Cursor> {
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return mRestaurantesDbHelper.getRestauranteById(mRestauranteId);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            if (cursor != null && cursor.moveToLast()) {
+                showRestaurante(new Restaurante(cursor));
+            } else {
+                showLoadError();
+            }
+        }
+
+    }
+    private void editRestaurante() {
+        boolean error = false;
+        String nombre=txtnombre.getText().toString();
+        if (TextUtils.isEmpty(nombre)) {
+            Toast.makeText(getApplicationContext(),
+                    "Rellene el campo nombre", Toast.LENGTH_SHORT).show();
+            error = true;
+        }
+        if(error)
+        {
+            return;
+        }
+        Restaurante restaurante = new Restaurante(nombre, "", "", foto, bar.getRating()+"",latitudfinal+"",longitudfinal+"");
+
+        new EditRestauranteTask().execute(restaurante);
+
+    }
+    private class EditRestauranteTask extends AsyncTask<Restaurante, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Restaurante... restaurantes) {
+            return mRestaurantesDbHelper.updateRestaurante(restaurantes[0], mRestauranteId) > 0;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            showLawyersScreen(result);
+        }
+
+    }
+    private void showLawyersScreen(Boolean requery) {
+        if (!requery) {
+            showAddEditError();
+           setResult(Activity.RESULT_CANCELED);
+        } else {
+            setResult(Activity.RESULT_OK);
+        }
+
+       finish();
+    }
+    private void showAddEditError() {
+        Toast.makeText(getApplicationContext(),
+                "Error al agregar nueva información", Toast.LENGTH_SHORT).show();
+    }
+    void configure_button() {
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+
+                //noinspection MissingPermission
+
+                //latitud= location.getLatitude();
+                //longitud=location.getLongitude();
+                latitudfinal=latitud;
+                longitudfinal=longitud;
+                mMap.clear();
+                LatLng rest = new LatLng(latitud, longitud);
+                mMap.addMarker(new MarkerOptions().position(rest).title(String.valueOf(txtnombre.getText())));
+                CameraPosition cameraPosition = CameraPosition.builder()
+                        .target(rest)
+                        .zoom(11)
+                        .build();
+                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+        });
+    }
+    protected void onStart() {
+        gac.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        gac.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            updateUI(location);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EditarRestauranteActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            return;
+        }
+        Log.d(TAG, "onConnected");
+
+        Location ll = LocationServices.FusedLocationApi.getLastLocation(gac);
+        Log.d(TAG, "LastLocation: " + (ll == null ? "NO LastLocation" : ll.toString()));
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(gac, locationRequest, this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try{
+                        LocationServices.FusedLocationApi.requestLocationUpdates(gac, locationRequest, this);
+                    } catch (SecurityException e) {
+                        Toast.makeText(EditarRestauranteActivity.this, "SecurityException:\n" + e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(EditarRestauranteActivity.this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(EditarRestauranteActivity.this, "onConnectionFailed: \n" + connectionResult.toString(),
+                Toast.LENGTH_LONG).show();
+        Log.d("DDD", connectionResult.toString());
+    }
+
+    private void updateUI(Location loc) {
+        Log.d(TAG, "updateUI");
+        latitud=loc.getLatitude();
+        longitud=loc.getLongitude();
+
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.d(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        Log.d(TAG, "This device is supported.");
+        return true;
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                    }
+                });
+        dialog.show();
+    }
+    public void fotoEdit(View v)
+    {
+        Intent intent = null;
+        int code = 0;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        code = DESDE_CAMARA;
+        startActivityForResult(intent, code);
+    }
+    public void galeriaEdit(View v)
+    {
+        Intent intent = null;
+        int code = 0;
+        intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        code = DESDE_GALERIA;
+        startActivityForResult(intent, code);
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        boolean errorTomandoFoto = false;
+        Bitmap imagen = null;
+        try {
+            if (requestCode == DESDE_CAMARA) {
+                imagen = (Bitmap) data.getParcelableExtra("data");
+                foto=StringBitmap.BitMapToString(imagen);
+
+            }
+
+            if (requestCode == DESDE_GALERIA) {
+                Uri uri = data.getData();
+                imagen = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                String[] projection = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(projection[0]);
+                String picturePath = cursor.getString(columnIndex); // returns null
+                foto=picturePath;
+                cursor.close();
+            }
+        }catch (Exception e) {
+            errorTomandoFoto = true;
+        }
+        if (!errorTomandoFoto) {
+            img.setImageBitmap(imagen);
+
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ Definitiva/app/src/main/java/com/example/aleja/definitiva/RestauranteDbHelper.java
@@ -0,0 +1,123 @@
+package com.example.aleja.definitiva;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.aleja.definitiva.RestauranteContract.RestauranteEntry;
+/**
+ * Created by aleja on 13/02/2018.
+ */
+
+public class RestauranteDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Restaurantes.db";
+
+    public RestauranteDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + RestauranteEntry.TABLE_NAME + " ("
+                + RestauranteEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + RestauranteEntry.ID + " TEXT NOT NULL,"
+                + RestauranteEntry.NOMBRE + " TEXT NOT NULL,"
+                + RestauranteEntry.CATEGORIA + " TEXT,"
+                + RestauranteEntry.DESCRIPCION + " TEXT,"
+                + RestauranteEntry.FOTO + " TEXT ,"
+                + RestauranteEntry.VALORACION + " TEXT,"
+                + RestauranteEntry.LONGITUD + " TEXT,"
+                + RestauranteEntry.LATITUD + " TEXT,"
+                + "UNIQUE (" + RestauranteEntry.ID + "))");
+        // Contenedor de valores
+
+        mockData(db);
+    }
+    private void mockData(SQLiteDatabase sqLiteDatabase) {
+        mockRestaurante(sqLiteDatabase, new Restaurante("American Restaurant", "Americano", "Es una pasada, atienden muy bien y la comida muy buena.", "", "5", "37.399905", "-5.925305"));
+        mockRestaurante(sqLiteDatabase, new Restaurante("La torre de PIZZA", "Italiano", "He pedido una pizza y estaba muy buena, pero ha tardado un poco.", "", "3.5", "37.404661", "-5.994504"));
+
+
+      /*  mockRestaurante(sqLiteDatabase, new Restaurante("El kebab de la Meca", "Turco", "37.413740", "-5.965467", "8", "El trato muy bueno y no ha tardado nada en traernos la comida. Lo único malo es el precio, un poco caro pero todo estaba muy bueno., ""));
+
+        mockRestaurante(sqLiteDatabase, new Restaurante("La lata que cole pol los lincones", "Chino", "37.404510", "-5.973669", "5", "El trato es algo malo, no entienden bien nuestro idioma y algunas veces se equivocan en el pedido. El 5 se lo doy por que es un restaurante bonito y la comida no está mala", ""));
+
+        mockRestaurante(sqLiteDatabase, new Restaurante("OleWacamole", "Mexicano", "37.387287", "-5.959937", "8", "El trato es bueno y la comida bastante buena. No le doy un 10 por que algunas veces llega a tardar hasta 45 minutos en entregar el pedido.", ""));
+
+        mockRestaurante(sqLiteDatabase, new Restaurante("Zampón", "Española", "37.385313", "-5.971440", "7", "La comida es muy buena y tarda muy poco en llegar el pedido pero el trato con los trabajadores es un poco pobre.", ""));
+    */
+    }
+    public long mockRestaurante(SQLiteDatabase db, Restaurante Restaurante) {
+        return db.insert(
+                RestauranteEntry.TABLE_NAME,
+                null,
+                Restaurante.toContentValues());
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public long saveRestaurante(Restaurante restaurante) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.insert(
+                RestauranteEntry.TABLE_NAME,
+                null,
+                restaurante.toContentValues());
+
+    }
+    public Cursor getAllRestaurantes() {
+        if (RestaurantesActivity.spinner.getSelectedItem().equals("Ver todos")) {
+            return getReadableDatabase()
+                    .query(
+                            RestauranteEntry.TABLE_NAME,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null);
+        }
+        else {
+            String categoria= (String)RestaurantesActivity.spinner.getSelectedItem();
+            return getReadableDatabase().query(
+                    RestauranteEntry.TABLE_NAME,
+                    null,
+                    RestauranteEntry.CATEGORIA + " LIKE ?",
+                    new String[]{categoria},
+                    null,
+                    null,
+                    null);
+        }
+
+    }
+
+
+    public Cursor getRestauranteById(String restauranteId) {
+            Cursor c = getReadableDatabase().query(
+                    RestauranteEntry.TABLE_NAME,
+                    null,
+                    RestauranteEntry.ID + " LIKE ?",
+                    new String[]{restauranteId},
+                    null,
+                    null,
+                    null);
+        return c;
+    }
+    public int deleteRestaurante(String restauranteId) {
+        return getWritableDatabase().delete(
+                RestauranteEntry.TABLE_NAME,
+                RestauranteEntry.ID + " LIKE ?",
+                new String[]{restauranteId});
+    }
+    public int updateRestaurante(Restaurante restaurante, String restauranteId) {
+        return getWritableDatabase().update(
+                RestauranteEntry.TABLE_NAME,
+                restaurante.toContentValues(),
+                RestauranteEntry.ID + " LIKE ?",
+                new String[]{restauranteId}
+        );
+    }
+
+
+}
